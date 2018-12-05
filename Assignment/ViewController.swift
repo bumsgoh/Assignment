a89a754056e2f864cf69a5964cc98f28328e5da2
bumslap <kl9151@naver.com>
2018-12-06 00:52:57 +0900

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by 고상범 on 2018. 12. 6..
+//  Copyright © 2018년 고상범. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
